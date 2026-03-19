6895191571631903251076f5b77e3d8a78b077a3
Brendan Lynskey <brendanlynskey@googlemail.com>
2026-03-19 21:46:00 +0000

    Add Vivado synthesis results for Artix-7 (xc7a35tcpg236-1)

---
+++ synthesis_logs/synth_divider_fp32_restoring.tcl
@@ -0,0 +1,18 @@
+set_param general.maxThreads 4
+create_project -in_memory -part xc7a35tcpg236-1
+
+read_verilog -sv [list \
+    /home/brendan/synthesis_workspace/Floating_Point_Dividers/divider_fp32_restoring.sv \
+    /home/brendan/synthesis_workspace/Floating_Point_Dividers/fp32_classify.sv /home/brendan/synthesis_workspace/Floating_Point_Dividers/fp32_exception_check.sv /home/brendan/synthesis_workspace/Floating_Point_Dividers/fp32_round_rne.sv \
+]
+
+set xdc_file "/home/brendan/synthesis_workspace/Floating_Point_Dividers/synthesis_logs/clock_divider_fp32_restoring.xdc"
+set fp [open $xdc_file w]
+puts $fp "create_clock -period 10.000 -name CLK \[get_ports CLK\]"
+close $fp
+read_xdc $xdc_file
+
+synth_design -top divider_fp32_restoring -part xc7a35tcpg236-1
+
+report_utilization -file /home/brendan/synthesis_workspace/Floating_Point_Dividers/synthesis_logs/utilization_divider_fp32_restoring.rpt
+report_timing_summary -file /home/brendan/synthesis_workspace/Floating_Point_Dividers/synthesis_logs/timing_divider_fp32_restoring.rpt
